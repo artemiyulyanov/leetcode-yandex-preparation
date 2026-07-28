02a0afe7a57dc2bb57b7f0ef033daff24fe9dee9
artemiyulyanov <artemiyulyanov.career@gmail.com>
2026-07-28 03:06:47 +0300

    feat: merge sorted arrays (#88) task solved

---
+++ easy/merge-sorted-arrays/MergeSortedArrays.h
@@ -0,0 +1,17 @@
+//
+// Created by Артемий on 28/07/2026.
+//
+
+#ifndef LEETCODE_YANDEX_PREPARATION_MERGESORTEDARRAYS_H
+#define LEETCODE_YANDEX_PREPARATION_MERGESORTEDARRAYS_H
+
+#include <iostream>
+#include <vector>
+
+class MergeSortedArrays {
+public:
+    static void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n);
+};
+
+
+#endif //LEETCODE_YANDEX_PREPARATION_MERGESORTEDARRAYS_H
