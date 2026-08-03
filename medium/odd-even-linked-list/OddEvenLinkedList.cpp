caa41e0e4133b18eb565e9f43f66df20136d2db3
artemiyulyanov <artemiyulyanov.career@gmail.com>
2026-08-03 05:08:48 +0300

    feat: odd even linked list (#328) task solved

---
+++ medium/odd-even-linked-list/OddEvenLinkedList.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Артемий on 03/08/2026.
+//
+
+#include "OddEvenLinkedList.h"
+
+ListNode *OddEvenLinkedList::oddEvenList(ListNode *head) {
+    if (head == nullptr || head->next == nullptr) return head;
+
+    ListNode* odd = head;
+    ListNode* even = head->next;
+    ListNode* evenHead = even;
+
+    while (even != nullptr && even->next != nullptr) {
+        odd->next = even->next;
+        odd = odd->next;
+
+        even->next = odd->next;
+        even = even->next;
+    }
+
+    odd->next = evenHead;
+
+    return head;
+}
